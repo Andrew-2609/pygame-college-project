ad30db4f84b3da12778774c0dfb38066f54fb084
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-12-03 11:09:33 -0300

    feat: add generated classes from diagram

---
+++ Model/EntityFactory.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class EntityFactory:
+    def __init__(self):
+        pass
+
+    def get_player(self, ):
+        pass
+
+    def get_enemy(self, ):
+        pass
+
+    def get_background(self, ):
+        pass
+
