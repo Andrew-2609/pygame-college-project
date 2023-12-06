d490f7f13d5f9f040948738c8bb9d7646b697a2b
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-12-05 22:09:12 -0300

    feat: turn Entity into an abstract class and define its initial behavior

--- code/Entity.py
+++ code/Entity.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surface = None
-        self.rect = None
+import pygame.image
+from pygame import Surface, Rect
 
+
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple = (0, 0)):
+        self.name: str = name
+        self.surface: Surface = pygame.image.load('./asset/' + name + '.png')
+        self.rect: Rect = self.surface.get_rect(left=position[0], top=position[1])
+        self.speed = 0
+
+    @abstractmethod
     def move(self, ):
         pass
-
