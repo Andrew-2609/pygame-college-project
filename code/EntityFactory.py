046b0f712a131554ae6ac00c4a0470730570e460
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-12-05 22:10:25 -0300

    feat: setup static method 'get_background' of EntityFactory

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+from code.Background import Background
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
-
     def get_player(self, ):
         pass
 
     def get_enemy(self, ):
         pass
 
-    def get_background(self, ):
-        pass
-
+    @staticmethod
+    def get_background(name: str, position: tuple = (0, 0)):
+        return Background(name, position)
